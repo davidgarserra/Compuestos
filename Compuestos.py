f3a9cda5289138b4967d4996c24e63b0cf582dac
david garcia <davidgarserra@hotmail.es>
2020-05-03 13:21:44 +0200

    calculo de los esfuerzos debidos a la temperatura

--- Compuestos.py
+++ Compuestos.py
@@ -8,6 +8,10 @@ X_c =1300       # Resistencia a compresion MPA
 Y_t=61          # resistencia a traccion en direccion transversal MPA
 Y_c =245        # resistencia a compresion en direccion transversa MPA
 S =108.85       # resistencia a coratadura MPA
+alpha_1 =-1E-6  #coef dilatacion en la direccion fibra
+alpha_2=26E-6   #coef dilatacion en la direccion transversal
+T_c =180        #temperatura de curado
+T_uso =23       #temperatura de uso
 
 #matrices Q 
 #(1) Lamina de 30 
@@ -58,6 +62,41 @@ B =1/2*calculo_suma(Q,dz2) # por t**2
 dz3 =np.diff(z_v**3)     #vector de diferencias al cubo
 
 D =1/3*calculo_suma(Q,dz3)  #por t**3
+                  
+# Calculo de las deformacion asociadas a la temperatura 
+
+def rotar_vector(v,ang):
+    '''Devuelve la matriz M rotada con angulo ang en grados,
+    transforma un vector en ejes de ortotropia a ejes del laminado'''
+    ang =np.pi/180*ang  #transformacion a radianes
+    M =np.array([[np.cos(ang)**2,np.sin(ang)**2,-2*np.sin(ang)*np.cos(ang)],
+                [np.sin(ang)**2,np.cos(ang)**2,2*np.sin(ang)*np.cos(ang)],
+                [np.sin(ang)*np.sin(ang),-np.sin(ang)*np.cos(ang),np.cos(ang)**2-np.sin(ang)**2]])
+    v_rot =M @ v
+    return v_rot 
+
+# calculo de los coeficientes en ejes del laminado
+
+angulos =np.array([30,90,90,-30])
+
+alphas =np.zeros((3,4))
+alpha_v =np.array([alpha_1,alpha_2,0])
+
+for i in range(4):
+    alphas[:,i]=rotar_vector(alpha_v,angulos[i])
+print(alphas)
+
+Delta_T =T_uso-T_c 
+N =np.zeros((1,3))
+
+for i in range(4):
+    N +=Q[i,:,:]  @ alphas[:,i] *dz[i]*Delta_T
+    
+print(N)
+
+
+
+
 
 
 
